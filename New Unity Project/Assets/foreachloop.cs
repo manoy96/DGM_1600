72d28f40f2fa3e19d823a94fe662527b448d7a4a
Manuel Espinoza <mfewcar@gmail.com>
2016-02-05 09:13:22 -0700

    2/5/16

---
+++ New Unity Project/Assets/foreachloop.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class foreachloop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+		string[] strings = new string[3];
+
+		strings[0] = "string1";
+		strings[1] = "string2";
+		strings[2] = "string3";
+
+		foreach (string item in strings) 
+		{
+			print (item);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
