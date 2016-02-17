8a54dbd6e1c7ba6be6f5b6f13701d23631525607
Manuel Espinoza <mfewcar@gmail.com>
2016-02-17 09:03:49 -0700

    scopeandaccessmodifiers

---
+++ New Unity Project/Assets/anotherclass.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anotherclass : MonoBehaviour 
+{
+
+	public int an94;
+	public int m4;
+
+	private int aug;
+	private int g36;
+
+	public void guns(int a,int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log ("guns total: " + answer);
+	}
+
+	private void ammo (int a,int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log ("ammo total: " + answer);
+	}
+
+
+	// Use this fsor initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ New Unity Project/Assets/scopeandaccessmodifier.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scopeandaccessmodifier : MonoBehaviour 
+{
+
+	public int alpha = 10;
+
+	private int beta = 0;
+	private int gamma = 15;
+
+	private anotherclass myOtherClass;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		alpha = 20;
+
+		myOtherClass = new anotherclass ();
+		myOtherClass.guns(alpha, myOtherClass.an94);
+
+	}
+
+	void example (int bullets, int mags)
+	{
+		int answer;
+		answer = bullets * mags * alpha;
+		Debug.Log (answer);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log ("alpha is set to" + alpha);
+	}
+}
